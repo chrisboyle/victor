28323de8d21a52c0b6045f76f78f2aa2d99dc1e2
Chris Boyle <chris@boyle.name>
2021-04-10 17:47:36 +0100

    Layout & infrastructure, most of the way there

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/chrisboyle/victor
+
+go 1.13
+
+require github.com/chrisboyle/victor-private v0.0.0-20210410200121-c03282f406db // indirect
---
+++ update-dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+BOOTSTRAP_VERSION="5.0.0-beta3"
+BOOTSTRAP_IN="https://cdn.jsdelivr.net/npm/bootstrap@$BOOTSTRAP_VERSION/dist/css/bootstrap.min.css"
+BOOTSTRAP_OUT=assets/css/bootstrap-$BOOTSTRAP_VERSION.min.css
+
+if [ ! -f $BOOTSTRAP_OUT ]; then
+  curl --create-dirs -o "$BOOTSTRAP_OUT" "$BOOTSTRAP_IN"
+fi
+
+hugo mod get -u
